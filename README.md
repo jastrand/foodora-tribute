451546f805bc2d826012c65aee00f77600e80557
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-03-05 22:43:40 +0100

    finished the project

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# foodora-tribute
-This is my tribute to the Foodora website
+Build a Food website 🌮🥑
+
+This was a project at Technigo Boot Camp. This website has been build using Zomato open API filitered on Vegetarian Food in New York.
+
+Focus: Javascript & API's! 
+
+
+Requirements:
+
+💰Filter on price range
+
+Add so that your users can filter restaurants based on different price ranges.
+
+🚀Sort on price
+
+Add so that your users can sort the resturants on price, cheap to expensive using the average_cost_for_two parameter.
+
+
+💬Filter on delivery options
+
+Make it so your users can choose to only show resturants which have delivery (has_online_delivery) or can be booked in advance (has_table_booking).
+
+📱Make the page responsive
--- script.js
+++ script.js
@@ -1,36 +1,75 @@
 
 
 const apiKey = "b8eed9fc27f7efae2b8240a39fe1c949";
-const cityId = 91; // Dublin
-const cuisineId = 82; //Pizza
-const resto = document.getElementById('restoInfo');
+const cityId = 280; // New York
+const cuisineId = 308; // Vegetarian
+let restoInfo;
+let averageCost;
+const priceLow = document.getElementById("lowPrice");
+const priceMedium = document.getElementById("mediumPrice");
+const priceHigh = document.getElementById("highPrice");
+const buttonFilter = document.getElementById("filterButton");
 
 fetch(
   `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&cuisines=${cuisineId}?`,
   { headers: { "user-key": apiKey } }
 )
   .then(response => {
-    return response.json();
+    return response.json()
+
   })
-  .then(json => {
-    console.log("API response:", json);
+  .then(info => {
+    console.log(info)
+    restoInfo = info.restaurants
+    showRestaurant(info.restaurants);
+  })
+const showRestaurant = (array) => {
+  console.log("show restau", array.length, array)
+  restaurants.innerHTML = `<p></p>`
+  array.forEach(item => {
+    restaurants.innerHTML += `<section class="card">
+    <img class="resto" src="${item.restaurant.thumb}">
+    <p><span class="restoname">${item.restaurant.name}</span><br> <span class="rating">
+    <img class="star" src="ic-star-sm.svg"> <span class="restorating">${item.restaurant.user_rating.aggregate_rating}/5 
+    (${item.restaurant.user_rating.votes})</span></span></p>
+    <p><span class="restocuisine"> ${item.restaurant.cuisines}</span>
+    </p></section>`
+  })
+}
 
-    json.restaurants.forEach(item => {
+// FILTER FUNCTIONS FOR DELIVERY/TAKE AWAY
+const filterFunction = () => {
+  const filteredArray = restoInfo.filter(item => item.restaurant.highlights.includes("Takeaway Available"))
+  console.log(filteredArray);
+  showRestaurant(filteredArray);
+};
+const filterDeliveryFunction = () => {
+  const filteredArray = restoInfo.filter(item => item.restaurant.highlights.includes("Delivery"))
+  console.log(filteredArray);
+  showRestaurant(filteredArray);
+};
 
-      resto.innerHTML += `
-      <img src="${item.restaurant.thumb}">
-      <p>${item.restaurant.name} 
-      <img src="ic-star-sm.svg"> ${item.restaurant.user_rating.aggregate_rating}/5 
-      (${item.restaurant.user_rating.votes})
-      </p>`
+//filters restaurants on price range
+const filterPrice = () => {
+  if (priceLow.checked) {
+    filteredArray = restoInfo.filter(
+      item => item.restaurant.average_cost_for_two <= 35
+    );
+  } else if (priceMedium.checked) {
+    filteredArray = restoInfo.filter(
+      item =>
+        item.restaurant.average_cost_for_two > 35 &&
+        item.restaurant.average_cost_for_two < 60
+    );
+  } else if (priceHigh.checked) {
+    filteredArray = restoInfo.filter(
+      item => item.restaurant.average_cost_for_two >= 60
+    );
+  }
 
-    });
-  });
+  showRestaurant(filteredArray);
+};
 
-  //console.log(`€${ item.restaurant.average_cost_for_two } `);
-      //console.log(item.restaurant.name);
-      //console.log(item.restaurant.user_rating.rating_text);
-      //console.log(item.restaurant.user_rating.aggregate_rating);
-      //console.log(item.restaurant.menu_url);
-      //console.log(item.restaurant.phone_numbers)
-      //console.log(item.restaurant.timings);
+document.getElementById('hasTakeaway').addEventListener('click', filterFunction)
+document.getElementById('hasDelivery').addEventListener('click', filterDeliveryFunction)
+buttonFilter.addEventListener("click", filterPrice);
